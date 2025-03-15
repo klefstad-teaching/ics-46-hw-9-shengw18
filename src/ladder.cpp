1843d52010686515058cb504818935c4f2332d9e
shengw18 <shengw18@uci.edu>
2025-03-15 09:29:08 +0000

    Commit Message Here

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -101,6 +101,7 @@ void print_word_ladder(const vector<string>& ladder) {
     if (ladder.empty()) {
         cout << "No word ladder found." << endl;
     } else {
+        cout << "Word ladder found: ";
         for (size_t i = 0; i < ladder.size(); i++) {
             if (i > 0) cout << " ";
             cout << ladder[i];
